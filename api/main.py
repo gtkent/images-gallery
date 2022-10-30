08e4e4518e29601463c7a5b645ffeab151d10deb
gtkent <kent.bouts@gmail.com>
2022-10-30 17:22:07 -0400

    Enabled pylint and black code formatter for py

--- api/main.py
+++ api/main.py
@@ -1,32 +1,34 @@
 from distutils.debug import DEBUG
-import requests, os
+import requests
+import os
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please create an .env.local file with your UNSPLASH_KEY")
+    raise EnvironmentError("Please create an .env.local file with your UNSPLASH_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG
+
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-      "Accept-Version": "v1",
-      "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050) 
+    app.run(host="0.0.0.0", port=5050)
